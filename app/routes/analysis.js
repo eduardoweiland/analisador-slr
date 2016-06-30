2914e0733853f9c9721825403bbb6b903560c77d
Eduardo Weiland <eduardo@eduardoweiland.info>
2016-06-29 21:31:50 -0300

    First steps into analysis

--- app/routes/analysis.js
+++ app/routes/analysis.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, inject: { service } } = Ember;
+
+export default Route.extend({
+  slrAnalysis: service('slr-analysis'),
+  provider: service('provider'),
+
+  model() {
+    let grammar = this.get('provider.grammar');
+    let augmented, itemized;
+
+    if (grammar && grammar.get('isValid')) {
+      augmented = this.get('slrAnalysis').augmentGrammar(grammar);
+      itemized = this.get('slrAnalysis').addItemMarkers(augmented);
+    }
+
+    return {grammar, augmented, itemized};
+  }
 });
